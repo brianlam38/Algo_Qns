d7746a1e93c916151a827c1c9180ebd2e7547d0b
brianlam38 <brian.lam38@gmail.com>
2017-02-02 15:35:52 +1100

    p67 bottleneck optimisation

--- p67_QN.c
+++ p67_QN.c
@@ -1,3 +1,8 @@
+// #############################
+// B.U.D OPTIMISATION TECHNIQUES
+// PART 1: Bottlenecks
+// #############################
+
 // Given an array of distinct integer values, count the number of pairs of integers
 // that have difference k.
 
@@ -16,6 +21,9 @@ int main(int argc, char *argv[]) {
 	return EXIT_SUCCESS;
 }
 
+// O(n^2) solution
+// The bottleneck here is REPEATED SEARCH for the "other side" of the pair.
+// This will be the main thing to optimize
 void intPairs(int *a, int k) {
 	for (int i = 0; i < sizeOfA ; i++) {
 		for (int j = 0; j < sizeOfA; j++) {
@@ -25,3 +33,39 @@ void intPairs(int *a, int k) {
 		}
 	}
 }
+
+// Optimized solution
+void intPairs(int *a, int k) {
+	// Sort the array = O(nlogn)
+	mergeSort(int *a);
+	// For each elt x, iterate until x+k = y is found, then go to the next elt.
+	for (int i = 0; i < sizeOfA; i++) {
+		for (int j = 0; j < sizeOfA; i++) {
+			if (a[i] + k == a[j]) {
+				printf("{%d,%d}", a[i], a[j]);
+				break;
+			}
+		}
+	}
+}
+
+// Suggested Optimized solution #1
+// 1. Sorting the array O(nLogn)
+// 2. Binary Search to find the "other side" of the pair O(nLogn)
+
+// Suggested Optimized solution #2
+// 1. Operate on an unsorted array using a HASH TABLE
+//    Put your array into a Hash Table.
+//	  To see if x+k or x-k exists in the array, simply look it up in the Hash Table O(N)
+	  i.e. if((a[i] + k) || (a[i] - k)) count++;
+	  // if x+k or x-k is TRUE, increment count
+// 2. Binary search to find the "other side" of the pair O(nLogn)
+
+
+
+
+
+
+
+
+
