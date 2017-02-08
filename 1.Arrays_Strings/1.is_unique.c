6041f4543760c63a1c78874afb069d40daaca1e2
brianlam38 <brian.lam38@gmail.com>
2017-02-08 15:43:34 +1100

    adding is_unique O(nlogn) method

--- 1.Arrays_Strings/1.is_unique.c
+++ 1.Arrays_Strings/1.is_unique.c
@@ -11,16 +11,7 @@ int duplicateStr(char *string);
 
 int main(int argc, char *argv[]) {
 
-	// Sort the string
-	// Iterate through the string. For each char, if next = prev, then the
-	// string will have a duplicate
-	// Runtime = O(nLogn) + O(n) = O(nLogn)
-
-	// LESS EFFICIENT METHOD:
-	// For each char, iterate through the string.
-	// If current char = iteration char, the string will have a duplicate
-
-	char string[] = "abcdefghijklmnopqrstuvwxyz";
+	char string[] = "abcdefghijklmnopqrstuvwxyzZASD";
 
 	if (duplicateStr(string)) {
 		printf("Duplicate");
@@ -31,19 +22,28 @@ int main(int argc, char *argv[]) {
 	return EXIT_SUCCESS;
 }
 
+// LESS EFFICIENT METHOD O(n^2)
+// For each char, iterate through the string.
+// If current char = iteration char, the string will have a duplicate
 int duplicateStr(char *string) {
 	for (int i = 0; string[i] != '\0'; i++) {
 		// printf("Testing for %c\n", string[i]);
 		char currStr = string[i];
 		int charCount = 0;
 		for (int j = 0; string[j] != '\0'; j++) {
-			// printf("Against: %c\n\n", string[j]);
+			// printf("Against: %c\n", string[j]);
 			if (currStr == string[j]) charCount++;
 			if (charCount > 1) return -1;
 		}
 	}
 	return 0;
 }
+
+// MORE EFFICIENT METHOD O(nLogn)
+// Iterate through the string. For each char, if next = prev, then the
+// string will have a duplicate
+// Runtime = O(nLogn) + O(n) = O(nLogn)
+
 
 
 
