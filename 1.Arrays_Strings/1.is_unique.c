daaccb4fe58d4bf5ef10ea3d0b0e4f97363bbff6
brianlam38 <brian.lam38@gmail.com>
2017-02-08 16:04:52 +1100

    updated is_unique O(n^2) algo

--- 1.Arrays_Strings/1.is_unique.c
+++ 1.Arrays_Strings/1.is_unique.c
@@ -6,13 +6,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <string.h>
 
 int duplicateStr(char *string);
+// int duplicateStrBetter(char *string);
 
 int main(int argc, char *argv[]) {
 
-	char string[] = "abcdefghijklmnopqrstuvwxyzZASD";
-
+	char string[] = "abcdefghijklmnopqrstuvwxyzZASD";	// declaring string literal
+														// contains implicit null term
 	if (duplicateStr(string)) {
 		printf("Duplicate");
 	} else {
@@ -28,22 +30,23 @@ int main(int argc, char *argv[]) {
 int duplicateStr(char *string) {
 	for (int i = 0; string[i] != '\0'; i++) {
 		// printf("Testing for %c\n", string[i]);
-		char currStr = string[i];
 		int charCount = 0;
 		for (int j = 0; string[j] != '\0'; j++) {
 			// printf("Against: %c\n", string[j]);
-			if (currStr == string[j]) charCount++;
+			if (strcmp(&string[i], &string[j]) == 0) charCount++;
 			if (charCount > 1) return -1;
 		}
 	}
 	return 0;
 }
-
-// MORE EFFICIENT METHOD O(nLogn)
-// Iterate through the string. For each char, if next = prev, then the
-// string will have a duplicate
-// Runtime = O(nLogn) + O(n) = O(nLogn)
-
+/*
+// MORE EFFICIENT METHOD O(nLogn) + O(n) = O(nlogn)
+// 1. Sort string
+// 2. Iterate through string. If curr = prev, then duplicate exists.
+int duplicateStrBetter(char *string) {
+	mergeSort(string);
+}
+*/
 
 
 
